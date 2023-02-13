1a0db189b2c1b9f06247c269dc9204883a91c866
gojimor <jgthjgth@gmail.com>
2023-02-13 21:24:33 +0300

    random id relocate to update planet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -10,15 +10,16 @@ export default class RandomPlanet extends Component {
   }
 
   state = {
-    id: Math.floor(Math.random() * 17) + 2,
+    id: null,
     name: null,
     population: null,
     rotationPeriod: null,
     diameter: null,
   };
   swapiService = new SwapiService();
+
   updatePlanet() {
-    const { id } = this.state;
+    const id = Math.floor(Math.random() * 17) + 2;
     this.swapiService.getPlanet(id).then((planet) => {
       this.setState({
         id,
@@ -29,33 +30,59 @@ export default class RandomPlanet extends Component {
       });
     });
   }
+
   render() {
     const { id, name, population, rotationPeriod, diameter } = this.state;
-    return (
-      <div className="random-planet jumbotron rounded">
-        <img
-          className="planet-image"
-          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-          alt="random-planet"
-        />
-        <div>
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
+    if (id) {
+      return (
+        <div className="random-planet jumbotron rounded">
+          <img
+            className="planet-image"
+            src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+            alt="random-planet"
+          />
+          <div>
+            <h4>{name}</h4>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <span className="term">Population</span>
+                <span>{population}</span>
+              </li>
+              <li className="list-group-item">
+                <span className="term">Rotation Period</span>
+                <span>{rotationPeriod}</span>
+              </li>
+              <li className="list-group-item">
+                <span className="term">Diameter</span>
+                <span>{diameter}</span>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="random-planet jumbotron rounded">
+          <img className="planet-image" alt="random-planet" />
+          <div>
+            <h4>{name}</h4>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <span className="term">Population</span>
+                <span>{population}</span>
+              </li>
+              <li className="list-group-item">
+                <span className="term">Rotation Period</span>
+                <span>{rotationPeriod}</span>
+              </li>
+              <li className="list-group-item">
+                <span className="term">Diameter</span>
+                <span>{diameter}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      );
+    }
   }
 }
